d8cfb39b2f580100695e437a6cae6f1e9489fa84
duartefellipe <duartefellipe@gmail.com>
2024-09-16 15:28:27 -0300

    Código para responder as perguntas dos exercícios 6 a 8

--- ExerciciosModuloII/main.py
+++ ExerciciosModuloII/main.py
@@ -1,9 +1,25 @@
-from utils import cran_reader
+from utils import cran_reader, search, search_by_word
 import os
 
 if __name__ == '__main__':
-	dataset_path = "../../../datasets/cranfield/cran.all.1400"
+	dataset_path = "../../../datasets/cranfield/"
 
-	index_time, cran_queue = cran_reader(dataset_path)
+	index_time, cran_queue = cran_reader(os.path.join(dataset_path, 'cran.all.1400'))
 	print("Tempo total: %2.5f, Tempo medio: %2.5f de %d docs armazenados "%(sum(index_time),sum(index_time)/len(cran_queue), len(cran_queue)))
 
+	queries_time, queries = cran_reader(os.path.join(dataset_path, 'cran.qry'))
+	search_time=[]
+	for (qi_pos, qi) in enumerate(queries):
+		qi_time, search_results = search(qi,cran_queue)
+		search_time.append(qi_time)
+#		print("q%d results: "%(qi_pos), search_results)
+		
+	print("Tempo total: %2.5f, Tempo medio: %2.5f para %d consultas"%(sum(queries_time+search_time), (sum(queries_time)+sum(search_time))/len(queries), len(queries)))
+
+	search_time=[]
+	for (qi_pos, qi) in enumerate(queries):
+		qi_time, search_results = search_by_word(qi,cran_queue)
+		search_time.append(qi_time)
+	#	print("q%d results: %d"%(qi_pos, len(search_results)))
+	
+	print("Tempo total: %2.5f, Tempo medio: %2.5f para %d consultas"%(sum(queries_time+search_time), (sum(queries_time)+sum(search_time))/len(queries), len(queries)))
--- ExerciciosModuloII/utils.py
+++ ExerciciosModuloII/utils.py
@@ -18,3 +18,25 @@ def cran_reader(file_to_read, encoding = "ISO-8859-1"):
 													
 													
 	return (read_time, queue_to_append)
+
+
+def search(query_to_search, data_queue):
+	start_time = time.time()
+	search_results = []
+
+	for (posi, di) in enumerate(data_queue):
+		if di == query_to_search:
+			search_results.append(posi)
+	return  (time.time() - start_time, search_results)
+
+
+def search_by_word(query_to_search, data_queue):
+	start_time = time.time()
+	search_results = set()
+
+	for (posi, di) in enumerate(data_queue):
+		tokens = query_to_search.split(" ")
+		for tki in tokens:
+			if tki in query_to_search:
+				search_results.add(posi)							
+	return  (time.time() - start_time, search_results)
