fda7e31a38c1f2b732bf8194c327f9e7560365c3
duartefellipe <duartefellipe@gmail.com>
2024-09-16 14:58:48 -0300

    codigo exercício 5 (exercicio 1 na cranfield)

--- ExerciciosModuloII/main.py
+++ ExerciciosModuloII/main.py
@@ -1,27 +1,9 @@
-from utils import folder_reader, search, search_by_word
+from utils import cran_reader
 import os
 
 if __name__ == '__main__':
-	dataset_path = "../../../datasets/spa/corpus-20090418"
+	dataset_path = "../../../datasets/cranfield/cran.all.1400"
 
-	index_time, spa_queue = folder_reader("/".join([dataset_path,"source", ]))
-	print("Tempo total: %2.5f, Tempo medio: %2.5f de %d docs armazenados "%(sum(index_time),sum(index_time)/len(spa_queue), len(spa_queue)))
+	index_time, cran_queue = cran_reader(dataset_path)
+	print("Tempo total: %2.5f, Tempo medio: %2.5f de %d docs armazenados "%(sum(index_time),sum(index_time)/len(cran_queue), len(cran_queue)))
 
-	queries_time, queries = folder_reader("/".join([dataset_path,"light", ]))
-	
-	search_time=[]
-	for (qi_pos, qi) in enumerate(queries):
-		qi_time, search_results = search(qi,spa_queue)
-		search_time.append(qi_time)
-		print("q%d results: "%(qi_pos), search_results)
-	
-	print("Tempo total: %2.5f, Tempo medio: %2.5f para buscar %d."%(sum(queries_time+search_time),(sum(queries_time)+sum(search_time))/len(queries),len(queries)))
-
-
-	search_time=[]
-	for (qi_pos, qi) in enumerate(queries):
-		qi_time, search_results = search_by_word(qi,spa_queue)
-		search_time.append(qi_time)
-		print("q%d results: %d"%(qi_pos, len(search_results)))
-	
-	print("Tempo total: %2.5f, Tempo medio: %2.5f para buscar %d."%(sum(queries_time+search_time),(sum(queries_time)+sum(search_time))/len(queries),len(queries)))
--- ExerciciosModuloII/utils.py
+++ ExerciciosModuloII/utils.py
@@ -3,42 +3,18 @@
 '''
 import os
 import time 
+import re
+
 def file_reader(path_to_read, encoding_to_read):
 	with open(path_to_read, encoding = encoding_to_read) as filei:
 		return filei.read()
 
-def folder_reader(folder_to_read, encoding = "ISO-8859-1"):
-	filenames = os.listdir(folder_to_read)
+def cran_reader(file_to_read, encoding = "ISO-8859-1"):
 	read_time = []
 	queue_to_append = []
-	for fnamei in filenames:
-		fpathi = "/".join([folder_to_read,fnamei])
-#		print(fpathi)
-		start_time = time.time()
-		queue_to_append.append(file_reader(fpathi, encoding))
-		read_time.append(time.time() - start_time)
+	start_time = time.time()
+	queue_to_append = re.split(".I [0-9]+\n",file_reader(file_to_read, encoding))[1:]
+	read_time.append(time.time() - start_time)
 													
 													
 	return (read_time, queue_to_append)
-
-def search(query_to_search, data_queue):
-	start_time = time.time()
-	search_results = []
-
-	for (posi, di) in enumerate(data_queue):
-		if di == query_to_search:
-			search_results.append(di)
-			print("achou!")							
-	return  (time.time() - start_time, search_results)
-
-
-def search_by_word(query_to_search, data_queue):
-	start_time = time.time()
-	search_results = set()
-
-	for (posi, di) in enumerate(data_queue):
-		tokens = query_to_search.split(" ")
-		for tki in tokens:
-			if tki in query_to_search:
-				search_results.add(di)							
-	return  (time.time() - start_time, search_results)
