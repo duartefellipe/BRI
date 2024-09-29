a27a1958b91dfca09df1ca5b73541862237f4f08
duartefellipe <duartefellipe@gmail.com>
2024-09-29 19:43:25 -0300

    Exercícios 9 a 12 do módulo II

--- ExerciciosModuloII/main.py
+++ ExerciciosModuloII/main.py
@@ -1,25 +1,25 @@
-from utils import cran_reader, search, search_by_word
+from utils import  search, search_by_word, folder_reader, query_reader
 import os
 
 if __name__ == '__main__':
-	dataset_path = "../../../datasets/cranfield/"
+	dataset_path = "../../../datasets/cf/cfc-xml"
+	index_time, cf_queue = folder_reader(dataset_path)
+	print("Tempo total: %2.5f, Tempo medio: %2.5f de %d docs armazenados "%(sum(index_time),sum(index_time)/len(cf_queue), len(cf_queue)))
 
-	index_time, cran_queue = cran_reader(os.path.join(dataset_path, 'cran.all.1400'))
-	print("Tempo total: %2.5f, Tempo medio: %2.5f de %d docs armazenados "%(sum(index_time),sum(index_time)/len(cran_queue), len(cran_queue)))
 
-	queries_time, queries = cran_reader(os.path.join(dataset_path, 'cran.qry'))
+	queries = query_reader(os.path.join(dataset_path, "cfquery-corrigido.xml"))
 	search_time=[]
 	for (qi_pos, qi) in enumerate(queries):
-		qi_time, search_results = search(qi,cran_queue)
+		qi_time, search_results = search(qi,cf_queue)
 		search_time.append(qi_time)
 #		print("q%d results: "%(qi_pos), search_results)
 		
-	print("Tempo total: %2.5f, Tempo medio: %2.5f para %d consultas"%(sum(queries_time+search_time), (sum(queries_time)+sum(search_time))/len(queries), len(queries)))
+	print("Tempo total: %2.5f, Tempo medio: %2.5f para %d consultas"%(sum(search_time), sum(search_time)/len(queries), len(queries)))
 
 	search_time=[]
 	for (qi_pos, qi) in enumerate(queries):
-		qi_time, search_results = search_by_word(qi,cran_queue)
+		qi_time, search_results = search_by_word(qi,cf_queue)
 		search_time.append(qi_time)
 	#	print("q%d results: %d"%(qi_pos, len(search_results)))
 	
-	print("Tempo total: %2.5f, Tempo medio: %2.5f para %d consultas"%(sum(queries_time+search_time), (sum(queries_time)+sum(search_time))/len(queries), len(queries)))
+print("Tempo total: %2.5f, Tempo medio: %2.5f para %d consultas"%(sum(search_time), sum(search_time)/len(queries), len(queries)))
--- ExerciciosModuloII/utils.py
+++ ExerciciosModuloII/utils.py
@@ -5,20 +5,47 @@ import os
 import time 
 import re
 
-def file_reader(path_to_read, encoding_to_read):
-	with open(path_to_read, encoding = encoding_to_read) as filei:
-		return filei.read()
 
-def cran_reader(file_to_read, encoding = "ISO-8859-1"):
+def extract_tag_content(tag_name, xml_content):
+		xml_pattern = "(<"+tag_name+">.*?<\\/"+tag_name+">)"		
+		return re.findall(xml_pattern,xml_content, flags = re.DOTALL)
+    
+def file_reader(path_to_read, encoding_to_read):
+	queue_to_append = []
+	with open(path_to_read, encoding = encoding_to_read) as filei:
+		xml_content = filei.read()			
+		for ri in extract_tag_content("RECORD", xml_content):
+			try:
+				title = extract_tag_content("TITLE", ri)
+				abstract = extract_tag_content("ABSTRACT", ri)
+				if len(abstract) == 0:
+					abstract = extract_tag_content("EXTRACT", ri)
+				queue_to_append.append(title[0]+abstract[0])
+			except:
+				pass
+
+		return queue_to_append
+	
+			
+def folder_reader(folder_to_read, encoding = "ISO-8859-1"):
+	filenames = os.listdir(folder_to_read)
 	read_time = []
 	queue_to_append = []
-	start_time = time.time()
-	queue_to_append = re.split(".I [0-9]+\n",file_reader(file_to_read, encoding))[1:]
-	read_time.append(time.time() - start_time)
-													
-													
+	for fnamei in filenames:
+		if "corrigido" in fnamei and not ("query" in fnamei) :
+			fpathi = os.path.join(folder_to_read,fnamei)
+			start_time = time.time()
+			queue_to_append = queue_to_append + file_reader(fpathi, encoding)
+			
+			read_time.append(time.time() - start_time)													
 	return (read_time, queue_to_append)
 
+   
+def query_reader(path_to_read, encoding_to_read = "ISO-8859-1"):
+	with open(path_to_read, encoding = encoding_to_read) as filei:
+		xml_content = filei.read()			
+		return extract_tag_content("QueryText", xml_content)
+		
 
 def search(query_to_search, data_queue):
 	start_time = time.time()
